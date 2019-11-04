9a8a4b8452a48d5151c5350cfe35c8fb914337df
Michał Winiarski <michal.winiarski@devbrains.pl>
2019-11-04 22:22:10 +0100

    Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+
+var path = require('path');
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'index.js',
+    libraryTarget: 'commonjs2'
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src'),
+        exclude: /(node_modules|build)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/env']
+          }
+        }
+      }
+    ]
+  }
+};
